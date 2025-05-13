43f6f5d43ad2510be2aa0db000474d2f67269ab2
GitHub Oaelan <1djb23@naver.com>
2025-05-13 13:39:27 +0900

    34545

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,14 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
     rules: {
       "@typescript-eslint/naming-convention": [
         "error",
         {
           selector: "variable",
-          // format: ["camelCase", "UPPER_CASE"], // 변수는 camelCase 또는 UPPER_CASE
+          format: ["camelCase", "UPPER_CASE"], // 변수는 camelCase 또는 UPPER_CASE
           leadingUnderscore: "forbid", // _로 시작 금지 유무
           filter: {
             regex: "^(__filename|__dirname)$", // 예외 허용
